98aaf1555ab10c99d0173a956dd5c06a355f820a
laraveljun <xiaojunming@eelly.net>
2017-06-09 22:50:46 +0800

    edit

---
+++ resources/assets/js/hello.js
@@ -0,0 +1,10 @@
+require('./bootstrap');
+import Hello from './components/Hello'
+
+new Vue({
+    // 找到 hello.blade.php 中指定的掛載點元素
+    el: '#app',
+
+    // 使用我們建立的 Hello(.vue) 元件
+    components: { Hello }
+});
