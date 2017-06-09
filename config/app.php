a0bc9e7d64ba9e150cd9433984c3974e06a3f6a5
laraveljun <xiaojunming@eelly.net>
2017-06-09 23:20:09 +0800

    edit

--- config/app.php
+++ config/app.php
@@ -229,7 +229,8 @@ return [
         'Request' => Illuminate\Support\Facades\Request::class,
         'Response' => Illuminate\Support\Facades\Response::class,
         'Route' => Illuminate\Support\Facades\Route::class,
-        'Schema' => Illuminate\Support\Facades\Schema::class,
+        //'Schema' => Illuminate\Support\Facades\Schema::class,
+        'Schema'    => zedisdog\LaravelSchemaExtend\Schema::class, //表注释工具
         'Session' => Illuminate\Support\Facades\Session::class,
         'Storage' => Illuminate\Support\Facades\Storage::class,
         'URL' => Illuminate\Support\Facades\URL::class,
